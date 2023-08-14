85e529f5d185cf5d252a3d095b122be46f8b232f
hantaejun <plushan98@naver.com>
2023-08-14 16:00:12 +0900

    채팅 기능 구현

---
+++ SpringFinalProject/src/main/java/com/sist/dao/FoodDAO.java
@@ -0,0 +1,20 @@
+package com.sist.dao;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+import com.sist.mapper.*;
+import com.sist.vo.*;
+
+@Repository
+public class FoodDAO {
+	@Autowired
+	private FoodMapper mapper;
+	public List<CategoryVO> foodCategoryListData()
+	{
+		return mapper.foodCategoryListData();
+	}
+}
